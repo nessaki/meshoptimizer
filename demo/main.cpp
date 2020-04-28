221f853efba0560af73f623818aa2a00ce5227c0
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2020-04-28 08:43:20 -0700

    clusterizer: Prioritize dead-end triangles
    If a triangle has one of the vertices stuck in a dead-end, we prioritize
    it over any other triangle that needs new vertices added to the meshlet;
    this can result in less full meshlets in some odd cases but is more
    globally optimal since it prevents suboptimal meshlets with 3
    vertices/triangle in the end of the traversal.

--- demo/main.cpp
+++ demo/main.cpp
@@ -1108,7 +1108,6 @@ void processDev(const char* path)
 	meshopt_optimizeVertexCache(&copy.indices[0], &copy.indices[0], copy.indices.size(), copy.vertices.size());
 
 	meshlets(copy, false);
-	meshlets(copy, true);
 }
 
 int main(int argc, char** argv)
